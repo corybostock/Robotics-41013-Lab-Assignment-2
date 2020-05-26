d2d3e1b8c64d11514472353551ecea530773884a
corybostock <cory.bostock@gmail.com>
2020-05-26 17:39:14 +1000

    fixed up the DH params and set a default q state.

--- main.m
+++ main.m
@@ -21,14 +21,14 @@ switch (mode)
        motion      = move();
        sawyer1     = sawyer(workSpace, 1, sawyerBase);
        % Init bodies
-       table       = body(workSpace, 'table', transl(0,0,floorOffset));            % Dimensions of the table (x, y, z) = (1.4880, 2.3383, 1.0896)
-       bowl        = body(workSpace, 'Bowl', transl(0,0,0));
-       mushroom    = body(workSpace, 'mushroom', transl(0.1,0,0));
-       tomato      = body(workSpace, 'tomato', transl(0.2,0,0));
-       carrot      = body(workSpace, 'carrot', transl(0.1,0.1,0));
-       lettuce     = body(workSpace, 'lettuce', transl(0.2,0.1,0));
-       onion       = body(workSpace, 'onion', transl(0.15,0.15,0));
-       tomatosauce = body(workSpace, 'tomatosauce', transl(0.2,0.15,0)); 
+%        table       = body(workSpace, 'table', transl(0,0,floorOffset));            % Dimensions of the table (x, y, z) = (1.4880, 2.3383, 1.0896)
+%        bowl        = body(workSpace, 'Bowl', transl(0,0,0));
+%        mushroom    = body(workSpace, 'mushroom', transl(0.1,0,0));
+%        tomato      = body(workSpace, 'tomato', transl(0.2,0,0));
+%        carrot      = body(workSpace, 'carrot', transl(0.1,0.1,0));
+%        lettuce     = body(workSpace, 'lettuce', transl(0.2,0.1,0));
+%        onion       = body(workSpace, 'onion', transl(0.15,0.15,0));
+%        tomatosauce = body(workSpace, 'tomatosauce', transl(0.2,0.15,0)); 
 end
 
 
