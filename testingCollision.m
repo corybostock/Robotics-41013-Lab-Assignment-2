da4a55af0216d5fbba097a42f3467a488c7ae846
cherrysodalady <41712449+cherrysodalady@users.noreply.github.com>
2020-06-05 20:54:09 +1000

    Collision detection working with movement

--- testingCollision.m
+++ testingCollision.m
@@ -8,105 +8,115 @@ clear
 hold on;
 floorOffset = (-1.0896/2);  
 workSpace = [-2.5 2.5 -2.5 2.5 (2*floorOffset) 2];
-sawyerBase = transl(0,0,1);
+sawyerBase = transl(0,0,0);
 motion = move();
 
 %put models in the workspace
 sawyer1 = sawyer(workSpace, 1, sawyerBase);
 hold on;
-table = body(workSpace, 'table', transl(0,0,0));
-%table = body(workSpace, 'table', transl(0,0,floorOffset));
-%r = 0.05
-%radii = [r,r,r]
+%table = body(workSpace, 'table', transl(0,0,1));
+table = body(workSpace, 'table', transl(0,0,floorOffset));
+r = 0.05
+radii = [r,r,r]
 
 %object faces and vertices
 tableFace = cell2mat(table.model.faces(1));
 tableVertex = cell2mat(table.model.points(1));
+tableVertex(:,3) = tableVertex(:,3)+floorOffset
 tableNormals = cell2mat(table.faceNormals(1));
 
+plot3(tableVertex(:,1),tableVertex(:,2), tableVertex(:,3))
 alpha(0.5)
 axis equal
 camlight
 
-%find each link and store their centers in a matrix
- tr = zeros(4,4,sawyer1.model.n+1);   %create a 4x4 matrix of each joint
- tr(:,:,1) = sawyer1.model.base;      %let the first matrix represent the base 
- L = sawyer1.model.links;
-% centers = zeros(7,3);
-% 
-% for i = 1 : sawyer1.model.n          %loop through and create a 4x4 matrix for each joint
-%     tr(:,:,i+1) = tr(:,:,i) * trotz(sawyer1.defaultq(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
-% end
-
-q = zeros(1,7)
-
-IsCollision(sawyer1.model, q, tableFace, tableVertex, tableNormals)
-return
-
-for i = 1 : size(tr,3)-1
-    for faceIndex = 1:size(tableFace{1},1)
-        vertOnPlane = tableVertex{1}(tableFace{1}(faceIndex,1)',:);
-        [intersectP,check] = LinePlaneIntersection(tableNormals{1}(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
-        if check == 1 && IsIntersectionPointInsideTriangle(intersectP,tableVertex{1}(tableFace{1}(faceIndex,:)',:))
-            plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
-            display('Intersection')
-        end
-    end    
-end
-%% 
 %set up positions
-targetpos = transl(0.4,0.3,-0.5)
+targetpos = transl(-0.214,0.176,-0.155)
 tempq = sawyer1.model.ikcon(targetpos);
 trajectory = jtraj(sawyer1.model.getpos(), tempq ,50);
+%sawyer1.model.teach
+%% 
 
 
-
+%move the robot
 for step = 1:size(trajectory,1)
     q = trajectory(step,:);
-    sawyer1.model.animate(q);
     
+%     %find each link and store their centers in a matrix
+%     tr = zeros(4,4,sawyer1.model.n+1);   %create a 4x4 matrix of each joint
+%     tr(:,:,1) = sawyer1.model.base;      %let the first matrix represent the base
+%     L = sawyer1.model.links;
+% 
+%     for i = 1 : sawyer1.model.n          %loop through and create a 4x4 matrix for each joint
+%         tr(:,:,i+1) = tr(:,:,i) * trotz(sawyer1.defaultq(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
+% 
+%     end
+    
+    IsCollision(sawyer1.model, q, tableFace, tableVertex, tableNormals)
+    sawyer1.model.animate(q);
+      
 end
 
-%check for intersections
-for i = 1 : size(tr,3)-1    
-    for faceIndex = 1:size(faces,1)
-        vertOnPlane = vertex(faces(faceIndex,1)',:);
-        [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
-        if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
-            plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
-            display('Intersection');
-        end
-    end    
-end
 
-%% moving and collision checking
-%transform of every joint
-tr = zeros(4,4,sawyer1.model.n+1);   %create a 4x4 matrix of each joint
-tr(:,:,1) = sawyer1.model.base;      %let the first matrix represent the base 
-L = sawyer1.model.links;
-centers = zeros(7,3);
+ 
+%IsCollision(sawyer1.model, q, tableFace, tableVertex, tableNormals)
 
-for i = 1 : sawyer1.model.n          %loop through and create a 4x4 matrix for each joint
-    tr(:,:,i+1) = tr(:,:,i) * trotz(sawyer1.defaultq(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
-    temp = tr(:,:,i);
-    temp = temp(1:3,4)';
-    centers(i, :) = temp;
-end
 
+
+
+
+%% 
 %set up positions
-targetpos = transl(0.531441000000000,-0.902481000000000,0.252341000000000)
-tempq = sawyer1.model.ikcon(targetpos)
-trajectory = jtraj(sawyer1.model.getpos(), tempq ,50);
-
-%ellipsoid distances to the table
-
-
-for step = 1:size(trajectory,1)
-    q = trajectory(step,:);
-    sawyer1.model.animate(q);
-    
-end
-       
+% targetpos = transl(0.4,0.3,-0.5)
+% tempq = sawyer1.model.ikcon(targetpos);
+% trajectory = jtraj(sawyer1.model.getpos(), tempq ,50);
+% 
+% for step = 1:size(trajectory,1)
+%     q = trajectory(step,:);
+%     sawyer1.model.animate(q);
+%     
+% end
+% 
+% %check for intersections
+% for i = 1 : size(tr,3)-1    
+%     for faceIndex = 1:size(faces,1)
+%         vertOnPlane = vertex(faces(faceIndex,1)',:);
+%         [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
+%         if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
+%             plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
+%             display('Intersection');
+%         end
+%     end    
+% end
+% 
+% %% moving and collision checking
+% %transform of every joint
+% tr = zeros(4,4,sawyer1.model.n+1);   %create a 4x4 matrix of each joint
+% tr(:,:,1) = sawyer1.model.base;      %let the first matrix represent the base 
+% L = sawyer1.model.links;
+% centers = zeros(8,3);
+% %sawyer1.model.fkine(sawyer1.model.getpos())
+% for i = 1 : sawyer1.model.n          %loop through and create a 4x4 matrix for each joint
+%     tr(:,:,i+1) = tr(:,:,i) * trotz(sawyer1.defaultq(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
+%     temp = tr(:,:,i);
+%     temp = temp(1:3,4)';
+%     centers(i, :) = temp;
+% end
+% 
+% %set up positions
+% targetpos = transl(0.531441000000000,-0.902481000000000,0.252341000000000)
+% tempq = sawyer1.model.ikcon(targetpos)
+% trajectory = jtraj(sawyer1.model.getpos(), tempq ,50);
+% 
+% %ellipsoid distances to the table
+% 
+% 
+% for step = 1:size(trajectory,1)
+%     q = trajectory(step,:);
+%     sawyer1.model.animate(q);
+%     
+% end
+%        
 
 
 
@@ -166,3 +176,22 @@ end
    % temp = tr(:,:,i);
   %  temp = temp(1:3,4)';
  %   centers(i, :) = temp;
+ 
+ %     for i = 2: 8
+%         [X,Y,Z] = ellipsoid(centers(i,1), centers(i,2), centers(i,3), radii(1), radii(2), radii(3) );
+%         hold on;
+%         view(3)
+%         ellispoidAtOrigin_h = surf(X,Y,Z)
+%         alpha(0.1);
+%     end
+    %q = zeros(1,7)
+    
+    %         temp = tr(:,:,i);
+%         temp = temp(1:3,4)';
+%         centers(i, :) = temp;
+%         centers(8,:) = endEffect
+
+    centers = zeros(8,3);
+    endEffect = sawyer1.model.fkine(sawyer1.model.getpos())
+    endEffect = endEffect(1:3,4)'
+    
